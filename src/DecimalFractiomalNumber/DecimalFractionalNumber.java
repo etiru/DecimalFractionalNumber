3b0316b57a1d9ce61c37ed5a124e85200b3eda97
Timur Timergalin <etiru@yandex.ru>
2018-04-02 22:10:54 +0300

    begin create class

---
+++ src/DecimalFractiomalNumber/DecimalFractionalNumber.java
@@ -0,0 +1,20 @@
+package DecimalFractiomalNumber;
+
+
+public final class DecimalFractionalNumber {
+    private final String hole;
+    private final String fractional;
+
+    public DecimalFractionalNumber(String hole, String fractional){
+    this.hole = hole;
+    this.fractional = fractional;
+    }
+
+    public String getHole(){
+        return hole;
+    }
+    public String getFractional(){
+        return fractional;
+    }
+
+}
