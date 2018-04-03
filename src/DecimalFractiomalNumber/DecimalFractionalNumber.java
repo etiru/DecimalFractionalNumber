4faee96206caa1322ac73b3884cf19f0ff5466f2
Timur Timergalin <etiru@yandex.ru>
2018-04-03 03:19:28 +0300

    begin create constructor and method plus

--- src/DecimalFractiomalNumber/DecimalFractionalNumber.java
+++ src/DecimalFractiomalNumber/DecimalFractionalNumber.java
@@ -1,20 +1,67 @@
 package DecimalFractiomalNumber;
 
 
+import jdk.nashorn.internal.runtime.regexp.joni.Regex;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.List;
+
 public final class DecimalFractionalNumber {
-    private final String hole;
-    private final String fractional;
+    private final  ArrayList<Integer> wholeNumber;
+    private final  ArrayList<Integer> fractionalNumber;
 
-    public DecimalFractionalNumber(String hole, String fractional){
-    this.hole = hole;
-    this.fractional = fractional;
+    public DecimalFractionalNumber(String number){
+        this.wholeNumber = new ArrayList<>();
+        this.fractionalNumber= new ArrayList<>();
+        int numLength = number.length();
+        int commonIndex = number.indexOf(",");
+
+        for (int i = 0; i < commonIndex; i += 9){
+            if (!((commonIndex - i) < 18)) {
+                String part = number.substring(i, i + 9);
+                Integer partToInt = Integer.parseInt(part);
+                wholeNumber.add(partToInt);
+            }
+            else {
+                Integer residue = commonIndex - i * 2;
+                String lastPart = number.substring(i + 9, (i + 9) + residue);
+                Integer lastPartToInt = Integer.parseInt(lastPart);
+                wholeNumber.add(lastPartToInt);
+                break;
+            }
+        }
+
+//        for (int i = commonIndex + 1; i < numLength; i += 9){
+//            String part = number.substring(i, i + 9);
+//            Integer partToInt = Integer.parseInt(part);
+//            fractionalNumber.add(partToInt);
+//            System.out.println("i"+i);
+//            System.out.println("comonIndex" + commonIndex);
+
+//            if (numLength - (commonIndex + 1) - i < 18){
+//
+//                String lastPart =
+//
+//
+//
+//
+//                fractionalNumber.add(lastPartToInt);
+//            }
+//        }
+
     }
 
-    public String getHole(){
-        return hole;
-    }
-    public String getFractional(){
-        return fractional;
-    }
 
+    public ArrayList<Integer> getWholeNumber() {return wholeNumber;}
+    public ArrayList<Integer> getFractionalNumber(){return fractionalNumber;}
+
+    public DecimalFractionalNumber plus(DecimalFractionalNumber other){
+        return new DecimalFractionalNumber(
+                if (wholeNumber.size() >= other.wholeNumber.size()){
+
+        }
+
+        );
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,10 @@
+import DecimalFractiomalNumber.DecimalFractionalNumber;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Скоро здесь что-то будет классное:)");
+        DecimalFractionalNumber dc = new DecimalFractionalNumber("7,45874589652145");
+        System.out.println(dc.getWholeNumber());
+//        System.out.println(dc.getFractionalNumber());
     }
 
 }
